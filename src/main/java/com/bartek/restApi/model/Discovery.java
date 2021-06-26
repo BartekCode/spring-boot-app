5ba3ecc8f0f67464023dd35726ffe365345b6cfd
Bartek <szkudlarz@onet.pl>
2021-06-26 16:31:16 +0200

    Added first Entity Discovery and starting configuration

---
+++ src/main/java/com/bartek/restApi/model/Discovery.java
@@ -0,0 +1,26 @@
+package com.bartek.restApi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "discoveries")
+public class Discovery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String title;
+    @NotEmpty
+    private String description;
+}
